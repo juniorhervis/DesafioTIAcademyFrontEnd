88f376577b0f5a004c60e649e1f533ef7f402968
Edson Luiz Hervis Junior <juniorhervis1@gmail.com>
2021-12-23 14:31:38 -0300

    implementação de cadastro de serviços

--- src/views/Home/Servico/Cadastrar/index.js
+++ src/views/Home/Servico/Cadastrar/index.js
@@ -1,7 +1,55 @@
+import axios from "axios";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Container } from "reactstrap";
+import { Button, Container, Form, FormGroup, Input, Label, Alert } from "reactstrap";
+import { api } from "../../../../config";
 
 export const CadastrarServico = () => {
+  const [servico, setServico] = useState({
+    nome: "",
+    descricao: "",
+  });
+
+  const [status, setStatus] = useState({
+    type: "",
+    message: "",
+  });
+
+  const valorInput = (e) =>
+    setServico({
+      ...servico,
+      [e.target.name]: e.target.value,
+    });
+
+  const cadServico = async (e) => {
+    e.preventDefault();
+
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    await axios
+      .post(api + "/servicos", servico, { headers })
+      .then((response) => {
+        if (response.data.error) {
+          setStatus({
+            type: "error",
+            message: response.data.message,
+          });
+        } else {
+          setStatus({
+            type: "success",
+            message: response.data.message,
+          });
+        }
+      })
+      .catch(() => {
+        setStatus({
+            type: "404",
+            message: "Sem conexão com a API.",
+          });
+      });
+  };
+
   return (
     <Container>
       <div className="d-flex">
@@ -14,6 +62,51 @@ export const CadastrarServico = () => {
           </Link>
         </div>
       </div>
+      <hr className="m-1" />
+
+      {status.type === "error" ? (
+        <Alert color="danger">{status.message}</Alert>
+      ) : (
+        ""
+      )}
+
+      {status.type === "success" ? (
+        <Alert color="success">{status.message}</Alert>
+      ) : (
+        ""
+      )}
+
+{status.type === "404" ? (
+        <Alert color="danger">{status.message}</Alert>
+      ) : (
+        ""
+      )}
+
+      <Form className="p-2" onSubmit={cadServico}>
+        <FormGroup className="p-2">
+          <Label>Nome</Label>
+          <Input
+            name="nome"
+            required
+            placeholder="Nome do Serviço"
+            type="text"
+            onChange={valorInput}
+          />
+        </FormGroup>
+        <FormGroup className="p-2">
+          <Label>Descrição</Label>
+          <Input
+            name="descricao"
+            required
+            placeholder="Descrição do Serviço"
+            type="text"
+            onChange={valorInput}
+          />
+        </FormGroup>
+        <Button type="submit" outline color="success">
+          Cadastrar
+        </Button>
+      </Form>
     </Container>
   );
 };
